957bbcc99298823621621d0546bdfa22301db9c0
Jakob Odersky <jodersky@gmail.com>
2011-03-05 19:35:05 +0000

    Renamed `Vector3' to `Vector3D'.

--- src/vhc/Vector3D.h
+++ src/vhc/Vector3D.h
@@ -1,12 +1,12 @@
 /*
- * Vector3.h
+ * Vector3D.h
  *
  *  Created on: Mar 1, 2011
  *      Author: Jakob Odersky
  */
 
-#ifndef VECTOR3_H_
-#define VECTOR3_H_
+#ifndef VECTOR3D_H_
+#define VECTOR3D_H_
 
 #include <iostream>
 #include <sstream>
@@ -19,8 +19,8 @@ namespace vhc {
  *  values (x, y, z), once initialised, cannot be changed anymore.
  *  This highly facilitates reasoning about code using vectors.
  */
-class Vector3 {
-	friend std::ostream& operator<< (std::ostream& output, const Vector3& v);
+class Vector3D {
+	friend std::ostream& operator<< (std::ostream& output, const Vector3D& v);
 
 private:
 	double x;
@@ -29,12 +29,12 @@ private:
 
 public:
 
-	/** Creates a new instance of <code>Vector3</code>.
+	/** Creates a new instance of <code>Vector3D</code>.
 	 *  @param x x value of this vector
 	 *  @param y y value of this vector
 	 *  @param z z value of this vector
 	 */
-	Vector3(double x, double y, double z) {this->x = x; this->y = y; this->z = z;};
+	Vector3D(double x, double y, double z) {this->x = x; this->y = y; this->z = z;};
 
 	/** Returns the x value of this vector. */
 	double getX() const {return x;};
@@ -46,37 +46,37 @@ public:
 	double getZ() const {return z;};
 
 	/** Tests if this vector and vector <code>v</code> are equal, i.e. have the same coordinates. */
-	bool operator== (const Vector3& v) const {return x == v.x && y == v.y && z == v.z;};
+	bool operator== (const Vector3D& v) const {return x == v.x && y == v.y && z == v.z;};
 
 	/** Tests if this vector and vector <code>v</code> are different, i.e. have different coordinates. */
-	bool operator!= (const Vector3& v) const {return !((*this) == (v));};
+	bool operator!= (const Vector3D& v) const {return !((*this) == (v));};
 
 	/** Vector addition. Returns a new vector resulting of adding this vector to vector <code>v</code>. */
-	Vector3 operator+ (const Vector3& v) const {return Vector3(x + v.x, y + v.y, z + v.z);};
+	Vector3D operator+ (const Vector3D& v) const {return Vector3D(x + v.x, y + v.y, z + v.z);};
 
 	/** Scalar multipication. Returns a new vector resulting of multiplying this vector by <code>n</code>. */
-	Vector3 operator* (double n) const {return Vector3(x * n, y * n, z * n);};
+	Vector3D operator* (double n) const {return Vector3D(x * n, y * n, z * n);};
 
 	/** Returns the opposite of this vector. */
-	Vector3 operator-() const {return (*this) * (-1.0);};
+	Vector3D operator-() const {return (*this) * (-1.0);};
 
 	/** Vector subtraction. Returns a new vector resulting of adding this vector to the opposite of vector <code>v</code>. */
-	Vector3 operator- (const Vector3& v) const {return (*this) + -v;};
+	Vector3D operator- (const Vector3D& v) const {return (*this) + -v;};
 
 	/** Scalar division. Returns a new vector resulting of multiplying this vector by the inverse of <code>n</code>. */
-	Vector3 operator/ (double n) const {return (*this) * (1.0 / n) ;};
+	Vector3D operator/ (double n) const {return (*this) * (1.0 / n) ;};
 
 	/** Dot product. Returns the dot product of this vector and vector <code>v</code>. */
-	double dot(const Vector3& v) const {return x * v.x + y * v.y + z * v.z;};
+	double dot(const Vector3D& v) const {return x * v.x + y * v.y + z * v.z;};
 
 	/** Cross product. Returns the right-handed cross product of this vector and vector <code>v</code>. */
-	Vector3 cross(const Vector3& v) const {return Vector3(y * v.z - v.y * z, v.x * z - x * v.z, x * v.y - v.x * y);};
+	Vector3D cross(const Vector3D& v) const {return Vector3D(y * v.z - v.y * z, v.x * z - x * v.z, x * v.y - v.x * y);};
 
 	/** Length of this vector. */
 	double getLength() const {return sqrt(dot(*this));};
 
 	/** Unit vector parallel to this vector. */
-	Vector3 getUnit() const {
+	Vector3D getUnit() const {
 		if (getLength() != 0.0) return (*this) / getLength();
 		else throw std::domain_error("Unit vector.");
 	};
@@ -84,24 +84,24 @@ public:
 	/** Returns a string representation of this vector. */
 	std::string toString() const {
 		std::stringstream s;
-		s << "Vector3(" << x << ", " << y << ", " << z << ")";
+		s << "Vector3D(" << x << ", " << y << ", " << z << ")";
 		return s.str();
 	};
 
 	/** Null vector. (0,0,0) */
-	static const Vector3 Null;
+	static const Vector3D Null;
 
 	/** X-axis unit vector. (1, 0, 0) */
-	static const Vector3 i;
+	static const Vector3D i;
 
 	/** Y-axis unit vector. (0, 1, 0) */
-	static const Vector3 j;
+	static const Vector3D j;
 
 	/** Z-axis unit vector. (0, 0, 1) */
-	static const Vector3 k;
+	static const Vector3D k;
 
 };
 
 } // namespace
 
-#endif /* VECTOR3_H_ */
+#endif /* VECTOR3D_H_ */
--- src/vhctest/Vector3DTest.cc
+++ src/vhctest/Vector3DTest.cc
@@ -1,5 +1,5 @@
 /*
- * Vector3Test.cc
+ * Vector3DTest.cc
  *
  *  Created on: Mar 1, 2011
  *      Author: Jakob Odersky
@@ -9,7 +9,7 @@
 #include <iostream>
 #include <assert.h>
 //TODO change relative include
-#include "../vhc/Vector3.h"
+#include "../vhc/Vector3D.h"
 
 using namespace std;
 using namespace vhc;
@@ -25,28 +25,28 @@ using namespace vhc;
 bool printTest();
 
 int main() {
-	cout << "Vector3: running tests..." << endl;
+	cout << "Vector3D: running tests..." << endl;
 	cout << flush;
 
 	//equality test
-	assert(Vector3(1, 2, 3) == Vector3(1, 2, 3));
+	assert(Vector3D(1, 2, 3) == Vector3D(1, 2, 3));
 
 	//addition test
-	assert(Vector3(1, -0.9, 57683) + Vector3(-1, 0.9, -57683) == Vector3::Null);
+	assert(Vector3D(1, -0.9, 57683) + Vector3D(-1, 0.9, -57683) == Vector3D::Null);
 
 	//addition, mutliplication test
-	assert(Vector3(4, 0, 16) / 4 == -Vector3(0.25, 0, 1) * -4);
+	assert(Vector3D(4, 0, 16) / 4 == -Vector3D(0.25, 0, 1) * -4);
 
 	//print test
 	//assert(printTest());
 
 	//length test
-	assert(Vector3(0,3,4).getLength() == 5);
-	assert(Vector3(0.0, 3.0, 4.0).getUnit().getLength() == 1);
+	assert(Vector3D(0,3,4).getLength() == 5);
+	assert(Vector3D(0.0, 3.0, 4.0).getUnit().getLength() == 1);
 
 	//unit test
-	Vector3 a = Vector3(0, 3, 4).getUnit();
-	Vector3 b = Vector3(0, 0.6, 0.8);
+	Vector3D a = Vector3D(0, 3, 4).getUnit();
+	Vector3D b = Vector3D(0, 0.6, 0.8);
 
 	cout << "unit: " << a << " std: " << b << endl;
 	cout << "x: " << (a.getX() == b.getX()) << endl;
@@ -54,20 +54,20 @@ int main() {
 	cout << "z: " << (a.getZ() == b.getZ()) << endl;
 
 
-	assert((Vector3(0, 3.0, 4.0).getUnit()) == Vector3(0, 0.6, 0.8));
+	assert((Vector3D(0, 3.0, 4.0).getUnit()) == Vector3D(0, 0.6, 0.8));
 
 
-	cout << "Vector3: tests completed successfully" << endl;
+	cout << "Vector3D: tests completed successfully" << endl;
 
 	return 0;
 }
 
 bool printTest() {
 	cout << "PRINT TEST" << endl;
-	cout << Vector3(1,2,3) << endl;
+	cout << Vector3D(1,2,3) << endl;
 	char answer;
 	do {
-		cout << "Was 'Vector3(1,2,3)' displayed? (y/n)" << endl;
+		cout << "Was 'Vector3D(1,2,3)' displayed? (y/n)" << endl;
 		cin >> answer;
 	} while (answer != 'y' && answer != 'n');
 	cout << "END PRINT TEST" << endl;
