cc0dec38cfebc47e687e97c548674da35f248ba8
Jakob Odersky <jodersky@gmail.com>
2011-03-28 17:39:39 +0000

    Resolu quelques erreurs du dernier commit.

--- src/main/CurvedElement.cc
+++ src/main/CurvedElement.cc
@@ -10,14 +10,10 @@
 #include "exceptions.h"
 #include "CurvedElement.h"
 
+using namespace std;
 namespace vhc {
 
-<<<<<<< .mine
 	/** Cf. CurvedElement.h */
-	CurvedElement::CurvedElement(const Vector3D& entry, const Vector3D& exit, double sectionRadius, double curvature, Element* next):
-
-using namespace std;
-
 CurvedElement::CurvedElement(const Vector3D& entry, const Vector3D& exit, double sectionRadius, double curvature, Element* next):
 
 		Element(entry, exit, sectionRadius, next),
@@ -34,24 +30,5 @@ CurvedElement::CurvedElement(const Vector3D& entry, const Vector3D& exit, double
 		Vector3D midpoint = (getEntryPosition() + getExitPosition())/ 2;
 		curvatureCenter = midpoint + 1 / k * sqrt(1.0 - (k * k) / 4 * getDiagonal().normSquare()) * (getDiagonal().unit().cross(Vector3D::k));
 }
-
-
-	/* TODO print also Vecotr3D magnetic field
-	* Schould we do as if curved element is an abstract class
-	* and implement this method under the class 'MagneticCurvedElement'??
-	*/
-	/** Cf. CurvedElement.h */
-	std::string CurvedElement::toString() const {
-
-		std::stringstream s;
-		s << "CurvedElement:"	<< "\n";
-		s << "\tPosition of entry: 	 	 "	<< entryPosition	<< "\n";
-		s << "\tPosition of exit: 	 	 "	<< exitPosition		<< "\n";
-		s << "\tRadius of section: 	 	 "	<< sectionRadius	<< "\n";
-		s << "\tRadius of curvature 1/k: "	<< 1/curvature		<< "\n";
-		s << "\tCenter of curvature:	 "	<< curvatureCenter	<< "\n";
-		s << "\t";
-		return s.str();
-	}
 
 }
